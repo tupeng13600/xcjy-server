02226fed9025399c6ad74176a739ba674b7eaed0
2267026134@qq.com <tupeng@13600>
2017-08-19 12:53:13 +0800

    修复部分BUG

--- xcjy-api/src/main/java/com/xcjy/web/common/util/DateUtil.java
+++ xcjy-api/src/main/java/com/xcjy/web/common/util/DateUtil.java
@@ -1,7 +1,10 @@
 package com.xcjy.web.common.util;
 
+import com.xcjy.web.common.exception.EducationException;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.LocalDate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
@@ -10,16 +13,23 @@ import java.util.Date;
  */
 public abstract class DateUtil {
 
-    public static Date getBirthByIdCard(String idCard) {
-        if(StringUtils.isBlank(idCard) || idCard.length() != 18) {
-            return null;
-        }
-        Integer year = Integer.parseInt(idCard.substring(6, 10));
-        Integer month = toInteger(idCard.substring(10, 12));
-        Integer day = toInteger(idCard.substring(12, 14));
+    private static Logger logger = LoggerFactory.getLogger(DateUtil.class);
 
-        LocalDate date = new LocalDate(year, month, day);
-        return date.toDate();
+    public static Date getBirthByIdCard(String idCard) {
+        try {
+            if (StringUtils.isBlank(idCard) || idCard.length() != 18) {
+                return null;
+            }
+            Integer year = Integer.parseInt(idCard.substring(6, 10));
+            Integer month = toInteger(idCard.substring(10, 12));
+            Integer day = toInteger(idCard.substring(12, 14));
+
+            LocalDate date = new LocalDate(year, month, day);
+            return date.toDate();
+        }catch (Exception e) {
+            logger.warn("身份证号码异常：{}", idCard);
+            throw new EducationException("身份证号码格式不正确");
+        }
     }
 
     public static Integer toInteger(String str){
